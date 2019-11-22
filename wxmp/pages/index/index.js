65a8dde576da0c351e5a8ae3b13d70ad65806327
takeshi <61330037@qq.com>
2019-11-22 15:50:55 +0800

    小程序端活动列表

--- wxmp/pages/index/index.js
+++ wxmp/pages/index/index.js
@@ -3,7 +3,45 @@
 
 Page({
   data: {
+    imgList: [],
+
+    imgUrls: [
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1623318287,3864173199&fm=27&gp=0.jpg',
+      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1623318287,3864173199&fm=27&gp=0.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
 
   },
-  onLoad: function() {}
+  jumpBtn:function(options){
+    wx.navigateTo({
+      url: '../detail/detail'
+    })
+  },
+  onLoad: function (options) {
+    for (let i = 0; i < 3; ++i) {
+      wx.request({
+        url: 'https://img.xjh.me/random_img.php',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        data: {
+          return: 'json',
+          type: 'bg',
+          ctype: 'acg'
+        },
+        success: res => {
+          let img = 'imgList[' + i + '].img'
+          this.setData({
+            [img]: res.data.img,
+          })
+        }
+      })
+    }
+    console.log(this.data.imgList)
+  },
+
+  
 })
