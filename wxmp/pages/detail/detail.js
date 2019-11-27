786de988164ba5c1ccbcb9bc0c11e2be213555db
takeshi <61330037@qq.com>
2019-11-27 18:51:49 +0800

    活动详情样式调整

--- wxmp/pages/detail/detail.js
+++ wxmp/pages/detail/detail.js
@@ -5,16 +5,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    //活动详情
+    picInfo:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
+    //  console.log(options)
+    let picId=options.id+'.jpg';
+    // this.getCurrentpic(picId);
   },
+// getCurrentpic(picId)
+// {
+//   let that=this;
+//   wx.request({
+//     url: 'http://140.143.129.182:80/images/?id='+picId,
+//     success(res) {
+//       console.log(res);
+//       if(res.data.code==0)
+//       {
+//         that.setData({
+//           picInfo:res.data.data.picInfo
+//         })
 
+//       }
+//     }
+//   })
+// },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -62,5 +81,18 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  //简介展开
+    handleExtensiontap: function (event) {
+    var readyData = {
+      "showAllDesc": true
+    };
+    this.setData(readyData);
+  },
+  bindPoster: function (event) {
+    var posterUrl = event.currentTarget.dataset.posterUrl;
+    wx.navigateTo({
+      url: '/pages/detail/de' 
+    });
+  },
 })
