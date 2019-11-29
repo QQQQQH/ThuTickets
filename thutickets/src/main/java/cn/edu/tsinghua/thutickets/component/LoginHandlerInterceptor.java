d1ed378a007e51712c1910748aea5a47be559bfe
QQQQQH <1295169310@qq.com>
2019-11-29 19:06:41 +0800

    修改活动列表为可排序的表格

--- thutickets/src/main/java/cn/edu/tsinghua/thutickets/component/LoginHandlerInterceptor.java
+++ thutickets/src/main/java/cn/edu/tsinghua/thutickets/component/LoginHandlerInterceptor.java
@@ -7,18 +7,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginHandlerInterceptor implements HandlerInterceptor {
 
-    @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response,
-                             Object handler) throws Exception {
-        Object username = request.getSession().getAttribute("username");
-        if (username != null && username.toString().equals("admin")) return true;
-        else {
-            request.setAttribute("msgWarning", "请先登录");
-            request.getRequestDispatcher("/admin/login").forward(request, response);
-            return false;
-        }
-    }
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,
+//                             HttpServletResponse response,
+//                             Object handler) throws Exception {
+//        Object username = request.getSession().getAttribute("username");
+//        if (username != null && username.toString().equals("admin")) return true;
+//        else {
+//            request.setAttribute("msgWarning", "请先登录");
+//            request.getRequestDispatcher("/admin/login").forward(request, response);
+//            return false;
+//        }
+//    }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {}
